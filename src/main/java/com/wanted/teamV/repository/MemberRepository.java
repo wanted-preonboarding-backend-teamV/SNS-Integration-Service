7e349171738c11ed261d42036d528bc4ac3ae620
shinmin <shinmin9812@naver.com>
2023-10-25 15:51:18 +0900

    feat/#6 Repository 구현 - MemberRepository, PostRepository, PostHistoryRepository, PostHashtagRepository 구현

---
+++ src/main/java/com/wanted/teamV/repository/MemberRepository.java
@@ -0,0 +1,11 @@
+package com.wanted.teamV.repository;
+
+import com.wanted.teamV.entity.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MemberRepository extends JpaRepository<Member, Long> {
+
+}
+
---
+++ src/main/java/com/wanted/teamV/repository/PostRepository.java
@@ -0,0 +1,11 @@
+package com.wanted.teamV.repository;
+
+import com.wanted.teamV.entity.Post;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PostRepository extends JpaRepository<Post, Long> {
+
+}
+
