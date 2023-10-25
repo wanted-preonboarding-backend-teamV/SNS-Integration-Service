23b9fc50766f14f5df83fbf8ac68f25066d2dd11
shinmin <shinmin9812@naver.com>
2023-10-25 11:57:46 +0900

    first commit

---
+++ src/test/java/com/wanted/teamV/TeamVApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wanted.teamV;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeamVApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
