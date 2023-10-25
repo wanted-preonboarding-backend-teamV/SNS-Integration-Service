4afd1bf735c885bdcc7a6aa7dc4ea2a90372a81e
jjungyeun <wjyddd@naver.com>
2023-10-25 22:56:12 +0900

    feat #10 - Enum 변환기 구현
    - API 파라미터로 Enum 클래스를 사용하기 위한 변환기 구현 및 설정
     (그냥 사용 시 소문자로 입력된 파라미터를 인식하지 못함)

---
+++ src/main/java/com/wanted/teamV/config/WebConfig.java
@@ -0,0 +1,16 @@
+package com.wanted.teamV.config;
+
+import com.wanted.teamV.type.converter.StringToStatisticsTimeTypeConverter;
+import com.wanted.teamV.type.converter.StringToStatisticsValueTypeConverter;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.format.FormatterRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addFormatters(FormatterRegistry registry) {
+        registry.addConverter(new StringToStatisticsValueTypeConverter());
+        registry.addConverter(new StringToStatisticsTimeTypeConverter());
+    }
+}
