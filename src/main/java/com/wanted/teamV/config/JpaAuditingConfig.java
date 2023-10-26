cdd81105d52a4eeec25542f4ea199bfd980eec3e
Nayoon <nayoonk928@gmail.com>
2023-10-27 06:27:07 +0900

    feat #15 - JPA Auditing 을 위한 JpaAuditingConfig 생성 - @EnableJpaAuditing 어노테이션을 사용하여 JPA Auditing 활성화 - 모듈화와 유지보수 향상을 위해 설정 파일로 분리

---
+++ src/main/java/com/wanted/teamV/config/JpaAuditingConfig.java
@@ -0,0 +1,9 @@
+package com.wanted.teamV.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
+public class JpaAuditingConfig {
+}
