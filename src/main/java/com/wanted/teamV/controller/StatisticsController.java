73d90582f7867b5d6f921a70b67b57e830bed0ca
jjungyeun <wjyddd@naver.com>
2023-10-26 12:04:56 +0900

    feat #10 - 통계 조회 API에 날짜 관련 로직 구현
    - startDate 또는 endDate가 없는 경우 기본값으로 설정하는 로직 구현
    - startDate > endDate인 경우 예외 처리
    - 최대 조회 기간을 넘어가는 경우 예외 처리
    - 기간 관련 예외 코드 추가

--- src/main/java/com/wanted/teamV/controller/StatisticsController.java
+++ src/main/java/com/wanted/teamV/controller/StatisticsController.java
@@ -1,6 +1,8 @@
 package com.wanted.teamV.controller;
 
 import com.wanted.teamV.dto.res.StatisticsResDto;
+import com.wanted.teamV.exception.CustomException;
+import com.wanted.teamV.exception.ErrorCode;
 import com.wanted.teamV.service.StatisticsService;
 import com.wanted.teamV.type.StatisticsSortType;
 import com.wanted.teamV.type.StatisticsTimeType;
@@ -10,12 +12,17 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
 import java.util.List;
 
 @RestController
 @RequestMapping("/statistics")
 @RequiredArgsConstructor
 public class StatisticsController {
+
+    private static final int MAX_DATE_PERIOD = 31;
+    private static final int MAX_HOUR_PERIOD = 7;
+
     private final StatisticsService statisticsService;
 
     @GetMapping
@@ -33,17 +40,35 @@ public class StatisticsController {
             // TODO - JWT 토큰에서 본인 계정 불러오기
             hashtag = "맛집";
         }
-        if (startDate == null) {
-            startDate = LocalDate.now().minusDays(7);
-        }
-        if (endDate == null) {
+
+        if (startDate == null && endDate == null) {
+            startDate = LocalDate.now().minusDays(MAX_HOUR_PERIOD -1);
             endDate = LocalDate.now();
+        }
+        else if (startDate == null) {
+            startDate = endDate.minusDays(MAX_HOUR_PERIOD -1);
+        }
+        else if (endDate == null) {
+            endDate = startDate.plusDays(MAX_HOUR_PERIOD -1);
+        }
+
+        if (endDate.isAfter(LocalDate.now())) {
+            endDate = LocalDate.now();
+        }
+
+        if (startDate.isAfter(endDate) || exceedMaxPeriod(startDate, endDate, timeType)) {
+            throw new CustomException(ErrorCode.INVALID_STATISTICS_DATE);
         }
 
         List<StatisticsResDto> responses =
                 statisticsService.getCountsForEachTimeByHashtag(hashtag, timeType, startDate, endDate, valueType, sortType);
 
         return ResponseEntity.ok(responses);
+    }
+
+    private boolean exceedMaxPeriod(LocalDate startDate, LocalDate endDate, StatisticsTimeType timeType) {
+        return (timeType == StatisticsTimeType.DATE && startDate.until(endDate, ChronoUnit.DAYS) > MAX_DATE_PERIOD - 1)
+                || (timeType == StatisticsTimeType.HOUR && startDate.until(endDate, ChronoUnit.DAYS) > MAX_HOUR_PERIOD - 1);
     }
 
 }
