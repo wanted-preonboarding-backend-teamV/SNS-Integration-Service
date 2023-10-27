f9ed147f835d29ddd94694d78bc4f0ed4e44e68e
shinmin <shinmin9812@naver.com>
2023-10-27 14:04:56 +0900

    feat #12 - PostHashtagRepository 수정 - findHashTagsByPostId() JPQL로 String 타입의 Hashtag 리스트 조회할 수 있도록 수정

--- src/main/java/com/wanted/teamV/repository/PostHashtagRepository.java
+++ src/main/java/com/wanted/teamV/repository/PostHashtagRepository.java
@@ -3,12 +3,14 @@ package com.wanted.teamV.repository;
 import com.wanted.teamV.entity.PostHashtag;
 import com.wanted.teamV.entity.PostHistory;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
 public interface PostHashtagRepository extends JpaRepository<PostHashtag, Long> {
+    @Query("SELECT ph.hashtag From PostHashtag ph WHERE ph.post.id = :postId")
     List<String> findHashTagsByPostId(Long postId);
 }
 
