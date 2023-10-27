38501c6610184c17134b3559ba30c6d689eb1386
jjungyeun <wjyddd@naver.com>
2023-10-27 15:15:07 +0900

    test #10 - 통계 API 컨트롤러 테스트 작성
    - 데이터에 시간 지정이 불가능해서 형식 및 오류 케이스만 확인함
    - type, sort에 따라 응답 형식이 알맞게 오는지 테스트
    - 조회 가능한 기간을 초과하는 경우 예외 테스트
    - hashtag값이 없는 경우 테스트

--- src/test/java/com/wanted/teamV/controller/StatisticsControllerTest.java
+++ src/test/java/com/wanted/teamV/controller/StatisticsControllerTest.java
@@ -1,16 +1,27 @@
 package com.wanted.teamV.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.jayway.jsonpath.JsonPath;
+import com.wanted.teamV.dto.req.MemberApproveReqDto;
+import com.wanted.teamV.dto.req.MemberJoinReqDto;
+import com.wanted.teamV.dto.req.MemberLoginReqDto;
+import com.wanted.teamV.exception.ErrorCode;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMapAdapter;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @AutoConfigureMockMvc
@@ -19,17 +30,135 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 class StatisticsControllerTest {
     @Autowired
     private MockMvc mockMvc;
+    @Autowired
+    private ObjectMapper objectMapper;
 
     @Test
-    public void getStatisticsWithDate() throws Exception {
+    @DisplayName("통계 조회 API의 응답 형식이 잘 맞는지 확인한다. (일자/시간 및 정렬")
+    public void response_format_test() throws Exception {
         MultiValueMapAdapter<String, String> params = new LinkedMultiValueMap<>();
         params.add("hashtag", "맛집");
+
+        // 일자, 내림차순
+        params.add("start", "2023-10-10");
+        params.add("end", "2023-10-13");
+
+        mockMvc.perform(MockMvcRequestBuilders.get("/statistics")
+                        .params(params))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.length()").value(4))
+                .andExpect(jsonPath("$[0].time").value("2023-10-13"))
+                .andDo(print());
+
+        // 일자, 오름차순
+        params.add("sort", "asc");
+
+        mockMvc.perform(MockMvcRequestBuilders.get("/statistics")
+                        .params(params))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.length()").value(4))
+                .andExpect(jsonPath("$[0].time").value("2023-10-10"))
+                .andDo(print());
+
+        // 시간, 오름차순
         params.add("type", "hour");
 
         mockMvc.perform(MockMvcRequestBuilders.get("/statistics")
-                .params(params))
+                        .params(params))
                 .andExpect(status().isOk())
+                .andExpect(jsonPath("$.length()").value(4 * 24))
+                .andExpect(jsonPath("$[0].time").value("2023-10-10T00:00:00"))
                 .andDo(print());
 
+        // 시간, 내림차순
+        params.remove("sort");
+        mockMvc.perform(MockMvcRequestBuilders.get("/statistics")
+                        .params(params))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.length()").value(4 * 24))
+                .andExpect(jsonPath("$[0].time").value("2023-10-13T00:00:00"))
+                .andDo(print());
+    }
+
+    @Test
+    @DisplayName("조회 가능 기간을 초과하여 조회에 실패한다.")
+    public void exceed_max_period() throws Exception {
+        // type=date인 경우 최대 31일
+        MultiValueMapAdapter<String, String> params = new LinkedMultiValueMap<>();
+        params.add("hashtag", "맛집");
+        params.add("start", "2022-10-01");
+        params.add("end", "2022-11-01");
+
+        mockMvc.perform(MockMvcRequestBuilders.get("/statistics")
+                .params(params))
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.errorCode").value(ErrorCode.INVALID_STATISTICS_DATE.name()))
+                .andDo(print());
+
+        // type=hour인 경우 최대 7일
+        params.add("type", "hour");
+        params.set("end", "2022-10-08");
+        mockMvc.perform(MockMvcRequestBuilders.get("/statistics")
+                        .params(params))
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.errorCode").value(ErrorCode.INVALID_STATISTICS_DATE.name()))
+                .andDo(print());
+    }
+
+    @Test
+    @DisplayName("hashtag 값이 없는 경우 토큰에서 사용자 정보를 구하여 계정을 대신 사용한다.")
+    public void no_hashtag_but_has_token() throws Exception {
+        // 회원가입
+        MemberJoinReqDto joinReqDto = new MemberJoinReqDto("qwertyasdf12345", "abc@gmail.com", "qwerty1234!");
+        MvcResult result = mockMvc.perform(post("/members")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(joinReqDto))
+                )
+                .andExpect(status().isOk())
+                .andReturn();
+
+        String response = result.getResponse().getContentAsString();
+        String code = JsonPath.parse(response).read("$.code");
+
+        // 가입승인
+        MemberApproveReqDto approveReqDto = new MemberApproveReqDto("qwertyasdf12345", "qwerty1234!", code);
+        mockMvc.perform(post("/members/approve")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(approveReqDto))
+                )
+                .andExpect(status().isNoContent());
+
+        // 로그인
+        MemberLoginReqDto loginReqDto = new MemberLoginReqDto("qwertyasdf12345", "qwerty1234!");
+        result = mockMvc.perform(post("/members/login")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(loginReqDto))
+                )
+                .andExpect(status().isOk())
+                .andReturn();
+
+        response = result.getResponse().getContentAsString();
+        String token = JsonPath.parse(response).read("$.accessToken");
+
+
+        // 통계 조회
+        MultiValueMapAdapter<String, String> params = new LinkedMultiValueMap<>();
+        params.add("start", "2022-10-01");
+        params.add("end", "2022-10-10");
+
+        mockMvc.perform(MockMvcRequestBuilders.get("/statistics")
+                .params(params)
+                .header("Authorization", token))
+                .andExpect(status().isOk())
+                .andDo(print());
+    }
+
+    @Test
+    @DisplayName("hashtag 값이 없는데 토큰도 없으면 요청에 실패한다.")
+    public void no_hashtag_and_token() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/statistics"))
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.errorCode").value(ErrorCode.INVALID_REQUEST.name()))
+                .andDo(print());
     }
 }
