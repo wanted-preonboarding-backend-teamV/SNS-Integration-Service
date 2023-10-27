2261001d0097390d2a6bf6682d2e29c137bd1be7
jjungyeun <wjyddd@naver.com>
2023-10-27 18:06:15 +0900

    feat #10 - 통계 API에서 인증토큰 받는 방식 변경
    - @AuthenticationPrincipal 어노테이션으로 사용자 아이디를 받아오도록 수정
    - 위 어노테이션이 동작하도록 WebConfig에 등록

--- src/main/java/com/wanted/teamV/controller/StatisticsController.java
+++ src/main/java/com/wanted/teamV/controller/StatisticsController.java
@@ -1,18 +1,21 @@
 package com.wanted.teamV.controller;
 
+import com.wanted.teamV.dto.LoginMember;
 import com.wanted.teamV.dto.res.StatisticsResDto;
 import com.wanted.teamV.entity.Member;
 import com.wanted.teamV.exception.CustomException;
 import com.wanted.teamV.exception.ErrorCode;
 import com.wanted.teamV.repository.MemberRepository;
-import com.wanted.teamV.service.MemberService;
 import com.wanted.teamV.service.StatisticsService;
 import com.wanted.teamV.type.StatisticsSortType;
 import com.wanted.teamV.type.StatisticsTimeType;
 import com.wanted.teamV.type.StatisticsValueType;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDate;
 import java.time.temporal.ChronoUnit;
@@ -27,23 +30,21 @@ public class StatisticsController {
     private static final int MAX_HOUR_PERIOD = 7;
 
     private final StatisticsService statisticsService;
-    private final MemberService memberService;
     private final MemberRepository memberRepository;
 
     @GetMapping
     public ResponseEntity<List<StatisticsResDto>> getStatistics(
-        @RequestHeader(value = "Authorization", required = false) String authToken,
+        @AuthenticationPrincipal LoginMember loginMember,
         @RequestParam(value = "hashtag", required = false) String hashtag,
         @RequestParam(value = "start", required = false) LocalDate startDate,
         @RequestParam(value = "end", required = false) LocalDate endDate,
         @RequestParam(value = "type", required = false, defaultValue = "date") StatisticsTimeType timeType,
         @RequestParam(value = "value", required = false, defaultValue = "count") StatisticsValueType valueType,
         @RequestParam(value = "sort", required = false, defaultValue = "desc") StatisticsSortType sortType
-
     ) {
 
         if (hashtag == null || hashtag.isBlank()) {
-            hashtag = getMemberAccount(authToken);
+            hashtag = getMemberAccount(loginMember.id());
         }
 
         if (startDate == null && endDate == null) {
@@ -71,11 +72,7 @@ public class StatisticsController {
         return ResponseEntity.ok(responses);
     }
 
-    private String getMemberAccount(String authToken) {
-        if (authToken == null || authToken.isBlank()) {
-            throw new CustomException(ErrorCode.INVALID_REQUEST);
-        }
-        Long memberId = memberService.extractUserId(authToken);
+    private String getMemberAccount(Long memberId) {
         Member member = memberRepository.findById(memberId)
                 .orElseThrow(() -> new CustomException(ErrorCode.INVALID_REQUEST));
         return member.getAccount();
