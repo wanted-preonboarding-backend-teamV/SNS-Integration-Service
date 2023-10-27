dff720300d1274683cbc36941379216ef9051471
jjungyeun <wjyddd@naver.com>
2023-10-27 15:13:01 +0900

    fix #10 - PostHistory에 EntityListener 설정 추가
    - @CreatedDate 어노테이션이 동작하도록 설정 추가

--- src/main/java/com/wanted/teamV/entity/PostHistory.java
+++ src/main/java/com/wanted/teamV/entity/PostHistory.java
@@ -8,12 +8,14 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
 
 @Entity
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@EntityListeners(AuditingEntityListener.class)
 public class PostHistory {
     @Id
     @Column(name = "history_id")
