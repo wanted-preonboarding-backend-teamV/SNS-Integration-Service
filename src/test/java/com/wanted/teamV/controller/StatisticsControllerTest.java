c6fb4a9f6d2f5bad07e6a3da25fb46d6448a61bf
jjungyeun <wjyddd@naver.com>
2023-10-27 18:07:43 +0900

    test #10 - 통계 API 컨트롤러 테스트에 인증 적용
    - API 요청 시 필요한 인증토큰을 발급받는 로직 추가
    - API 요청 시 인증토큰을 포함해서 요청
    - 불필요한 TC 삭제 (토큰이 없는 경우는 사용자 관련 테스트)

--- src/test/java/com/wanted/teamV/controller/StatisticsControllerTest.java
+++ src/test/java/com/wanted/teamV/controller/StatisticsControllerTest.java
@@ -6,6 +6,7 @@ import com.wanted.teamV.dto.req.MemberApproveReqDto;
 import com.wanted.teamV.dto.req.MemberJoinReqDto;
 import com.wanted.teamV.dto.req.MemberLoginReqDto;
 import com.wanted.teamV.exception.ErrorCode;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -33,81 +34,10 @@ class StatisticsControllerTest {
     @Autowired
     private ObjectMapper objectMapper;
 
-    @Test
-    @DisplayName("통계 조회 API의 응답 형식이 잘 맞는지 확인한다. (일자/시간 및 정렬")
-    public void response_format_test() throws Exception {
-        MultiValueMapAdapter<String, String> params = new LinkedMultiValueMap<>();
-        params.add("hashtag", "맛집");
+    private String accessToken;
 
-        // 일자, 내림차순
-        params.add("start", "2023-10-10");
-        params.add("end", "2023-10-13");
-
-        mockMvc.perform(MockMvcRequestBuilders.get("/statistics")
-                        .params(params))
-                .andExpect(status().isOk())
-                .andExpect(jsonPath("$.length()").value(4))
-                .andExpect(jsonPath("$[0].time").value("2023-10-13"))
-                .andDo(print());
-
-        // 일자, 오름차순
-        params.add("sort", "asc");
-
-        mockMvc.perform(MockMvcRequestBuilders.get("/statistics")
-                        .params(params))
-                .andExpect(status().isOk())
-                .andExpect(jsonPath("$.length()").value(4))
-                .andExpect(jsonPath("$[0].time").value("2023-10-10"))
-                .andDo(print());
-
-        // 시간, 오름차순
-        params.add("type", "hour");
-
-        mockMvc.perform(MockMvcRequestBuilders.get("/statistics")
-                        .params(params))
-                .andExpect(status().isOk())
-                .andExpect(jsonPath("$.length()").value(4 * 24))
-                .andExpect(jsonPath("$[0].time").value("2023-10-10T00:00:00"))
-                .andDo(print());
-
-        // 시간, 내림차순
-        params.remove("sort");
-        mockMvc.perform(MockMvcRequestBuilders.get("/statistics")
-                        .params(params))
-                .andExpect(status().isOk())
-                .andExpect(jsonPath("$.length()").value(4 * 24))
-                .andExpect(jsonPath("$[0].time").value("2023-10-13T00:00:00"))
-                .andDo(print());
-    }
-
-    @Test
-    @DisplayName("조회 가능 기간을 초과하여 조회에 실패한다.")
-    public void exceed_max_period() throws Exception {
-        // type=date인 경우 최대 31일
-        MultiValueMapAdapter<String, String> params = new LinkedMultiValueMap<>();
-        params.add("hashtag", "맛집");
-        params.add("start", "2022-10-01");
-        params.add("end", "2022-11-01");
-
-        mockMvc.perform(MockMvcRequestBuilders.get("/statistics")
-                .params(params))
-                .andExpect(status().isBadRequest())
-                .andExpect(jsonPath("$.errorCode").value(ErrorCode.INVALID_STATISTICS_DATE.name()))
-                .andDo(print());
-
-        // type=hour인 경우 최대 7일
-        params.add("type", "hour");
-        params.set("end", "2022-10-08");
-        mockMvc.perform(MockMvcRequestBuilders.get("/statistics")
-                        .params(params))
-                .andExpect(status().isBadRequest())
-                .andExpect(jsonPath("$.errorCode").value(ErrorCode.INVALID_STATISTICS_DATE.name()))
-                .andDo(print());
-    }
-
-    @Test
-    @DisplayName("hashtag 값이 없는 경우 토큰에서 사용자 정보를 구하여 계정을 대신 사용한다.")
-    public void no_hashtag_but_has_token() throws Exception {
+    @BeforeEach
+    void setUp() throws Exception {
         // 회원가입
         MemberJoinReqDto joinReqDto = new MemberJoinReqDto("qwertyasdf12345", "abc@gmail.com", "qwerty1234!");
         MvcResult result = mockMvc.perform(post("/members")
@@ -138,27 +68,99 @@ class StatisticsControllerTest {
                 .andReturn();
 
         response = result.getResponse().getContentAsString();
-        String token = JsonPath.parse(response).read("$.accessToken");
+        accessToken = JsonPath.parse(response).read("$.accessToken");
+    }
 
+    @Test
+    @DisplayName("통계 조회 API의 응답 형식이 잘 맞는지 확인한다. (일자/시간 및 정렬")
+    public void response_format_test() throws Exception {
+        MultiValueMapAdapter<String, String> params = new LinkedMultiValueMap<>();
+        params.add("hashtag", "맛집");
 
+        // 일자, 내림차순
+        params.add("start", "2023-10-10");
+        params.add("end", "2023-10-13");
+
+        mockMvc.perform(MockMvcRequestBuilders.get("/statistics")
+                        .params(params)
+                        .header("Authorization", "Bearer " + accessToken))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.length()").value(4))
+                .andExpect(jsonPath("$[0].time").value("2023-10-13"))
+                .andDo(print());
+
+        // 일자, 오름차순
+        params.add("sort", "asc");
+
+        mockMvc.perform(MockMvcRequestBuilders.get("/statistics")
+                        .params(params)
+                        .header("Authorization", "Bearer " + accessToken))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.length()").value(4))
+                .andExpect(jsonPath("$[0].time").value("2023-10-10"))
+                .andDo(print());
+
+        // 시간, 오름차순
+        params.add("type", "hour");
+
+        mockMvc.perform(MockMvcRequestBuilders.get("/statistics")
+                        .params(params)
+                        .header("Authorization", "Bearer " + accessToken))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.length()").value(4 * 24))
+                .andExpect(jsonPath("$[0].time").value("2023-10-10T00:00:00"))
+                .andDo(print());
+
+        // 시간, 내림차순
+        params.remove("sort");
+        mockMvc.perform(MockMvcRequestBuilders.get("/statistics")
+                        .params(params)
+                        .header("Authorization", "Bearer " + accessToken))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.length()").value(4 * 24))
+                .andExpect(jsonPath("$[0].time").value("2023-10-13T00:00:00"))
+                .andDo(print());
+    }
+
+    @Test
+    @DisplayName("조회 가능 기간을 초과하여 조회에 실패한다.")
+    public void exceed_max_period() throws Exception {
+        // type=date인 경우 최대 31일
+        MultiValueMapAdapter<String, String> params = new LinkedMultiValueMap<>();
+        params.add("hashtag", "맛집");
+        params.add("start", "2022-10-01");
+        params.add("end", "2022-11-01");
+
+        mockMvc.perform(MockMvcRequestBuilders.get("/statistics")
+                        .params(params)
+                        .header("Authorization", "Bearer " + accessToken))
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.errorCode").value(ErrorCode.INVALID_STATISTICS_DATE.name()))
+                .andDo(print());
+
+        // type=hour인 경우 최대 7일
+        params.add("type", "hour");
+        params.set("end", "2022-10-08");
+        mockMvc.perform(MockMvcRequestBuilders.get("/statistics")
+                        .params(params)
+                        .header("Authorization", "Bearer " + accessToken))
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.errorCode").value(ErrorCode.INVALID_STATISTICS_DATE.name()))
+                .andDo(print());
+    }
+
+    @Test
+    @DisplayName("hashtag 값이 없는 경우 토큰에서 사용자 정보를 구하여 계정을 대신 사용한다.")
+    public void no_hashtag_but_has_token() throws Exception {
         // 통계 조회
         MultiValueMapAdapter<String, String> params = new LinkedMultiValueMap<>();
         params.add("start", "2022-10-01");
         params.add("end", "2022-10-10");
 
         mockMvc.perform(MockMvcRequestBuilders.get("/statistics")
-                .params(params)
-                .header("Authorization", token))
+                        .params(params)
+                        .header("Authorization", "Bearer " + accessToken))
                 .andExpect(status().isOk())
-                .andDo(print());
-    }
-
-    @Test
-    @DisplayName("hashtag 값이 없는데 토큰도 없으면 요청에 실패한다.")
-    public void no_hashtag_and_token() throws Exception {
-        mockMvc.perform(MockMvcRequestBuilders.get("/statistics"))
-                .andExpect(status().isBadRequest())
-                .andExpect(jsonPath("$.errorCode").value(ErrorCode.INVALID_REQUEST.name()))
                 .andDo(print());
     }
 }
