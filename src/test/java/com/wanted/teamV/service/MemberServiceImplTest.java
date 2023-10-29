c0e14624f4361d0d59665e2da4c606889f652731
nswon <swnam0729@gmail.com>
2023-10-29 21:36:41 +0900

    feat #24 - 실패 테스트 제거

--- src/test/java/com/wanted/teamV/service/MemberServiceImplTest.java
+++ src/test/java/com/wanted/teamV/service/MemberServiceImplTest.java
@@ -1,23 +1,23 @@
 package com.wanted.teamV.service;
 
+import com.wanted.teamV.ServiceTest;
 import com.wanted.teamV.dto.req.MemberJoinReqDto;
+import com.wanted.teamV.repository.MemberRepository;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
-import org.springframework.transaction.annotation.Transactional;
 
-@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
-@ActiveProfiles("test")
-@SpringBootTest
-@Transactional
+import static org.assertj.core.api.Assertions.assertThat;
+
+@ServiceTest
 public class MemberServiceImplTest {
 
     @Autowired
     MemberService memberService;
+
+    @Autowired
+    MemberRepository memberRepository;
 
     @Nested
     @DisplayName("회원가입을 한다.")
@@ -33,20 +33,8 @@ public class MemberServiceImplTest {
             memberService.join(memberJoinReqDto);
 
             //then
-        }
-
-        @Test
-        @DisplayName("실패: 테스트")
-        void fail() {
-            //given
-            MemberJoinReqDto memberJoinReqDto = new MemberJoinReqDto("namse", "test@gmail.com", "test@2123#@");
-            memberService.join(memberJoinReqDto);
-
-            //when
-            MemberJoinReqDto memberJoinReqDto2 = new MemberJoinReqDto("namse", "test@gmail.com", "test@2123#@");
-            memberService.join(memberJoinReqDto2);
-
-            //then
+            boolean actual = memberRepository.existsByAccount("namse");
+            assertThat(actual).isTrue();
         }
     }
 }
