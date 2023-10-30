a4198f1ca5a24175c8d17de13912a62e8290e6c0
Nayoon <nayoonk928@gmail.com>
2023-10-30 20:24:13 +0900

    chore #22 - conflict 수정

--- src/test/java/com/wanted/teamV/service/PostServiceImplTest.java
+++ src/test/java/com/wanted/teamV/service/PostServiceImplTest.java
@@ -31,6 +31,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestTemplate;
 
+import java.net.URI;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
