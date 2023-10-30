63156b57af9d153cd5235a70d50296d3fb08fff5
Nayoon <nayoonk928@gmail.com>
2023-10-30 20:25:48 +0900

    chore #22 - conflict 수정

--- src/test/java/com/wanted/teamV/service/PostServiceImplTest.java
+++ src/test/java/com/wanted/teamV/service/PostServiceImplTest.java
@@ -24,12 +24,12 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
-
-import org.springframework.data.domain.*;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestTemplate;
+
+import org.springframework.data.domain.*;
 
 import java.net.URI;
 import java.util.Arrays;
